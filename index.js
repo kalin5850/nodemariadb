beb1ad1c33b69c8a7cbe75de4f72f0fefdd25ca1
kail <kail@tvws.com.tw>
2020-11-07 16:06:03 +0800

    async await query

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const conn = require("./models/db.js");
+
+app.get("/", (req, res) => {
+  conn.then(async (sql) => {
+    try {
+      const result_1 = await sql.query("select * from products");
+      // console.log(result_1);
+      JSON.parse(JSON.stringify(result_1));
+      res.send(result_1);
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  });
+});
+
+app.listen(port, () => console.log(`Example app listening on port port!`));
---
+++ models/db.js
@@ -0,0 +1,18 @@
+const mariadb = require("mariadb");
+const dbConfig = require("../config/db.config.js");
+const pool = mariadb.createPool({
+  host: dbConfig.dev.HOST,
+  user: dbConfig.dev.USER,
+  password: dbConfig.dev.PASSWORD,
+  database: dbConfig.dev.DB,
+  useConnectionPooling: true,
+});
+
+async function getDBConnection() {
+  let conn = await pool.getConnection();
+  // console.log(conn);
+  return conn;
+}
+
+module.exports = getDBConnection();
+// getDBConnection();
