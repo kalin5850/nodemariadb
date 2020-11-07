0d7435e8a9581e201d225d3f41ee3536cf4fef6d
kail <kail@tvws.com.tw>
2020-11-07 16:32:38 +0800

    add catch statement

--- index.js
+++ index.js
@@ -5,17 +5,20 @@ const port = 3000;
 const conn = require("./models/db.js");
 
 app.get("/", (req, res) => {
-  conn.then(async (sql) => {
-    try {
-      const result_1 = await sql.query("select * from products");
-      // console.log(result_1);
-      JSON.parse(JSON.stringify(result_1));
-      res.send(result_1);
-    } catch (err) {
-      console.log(err);
+  conn
+    .then(async (sql) => {
+      try {
+        const result_1 = await sql.query("select * from products");
+        // console.log(result_1);
+        JSON.parse(JSON.stringify(result_1));
+        res.send(result_1);
+      } catch (err) {
+        console.log(`Query error message: ${err}`);
+      }
+    })
+    .catch(async (err) => {
       throw err;
-    }
-  });
+    });
 });
 
-app.listen(port, () => console.log(`Example app listening on port port!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
